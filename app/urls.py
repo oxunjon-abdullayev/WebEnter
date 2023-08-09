24d0b3e4ca910e4ac1e3c3cde874215016ff3908
oxunjon <abdullayevoxunjon757@gmail.com>
2023-08-09 17:59:56 +0500

    blog blog deatail is done

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from app.views import IndexView
+from app.views import index_view, blogs_view, blog_detail
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', index_view, name='index'),
+    path('blog-page/', blogs_view, name='blog-view'),
+    path('blog-detail/<int:blog_id>/', blog_detail, name='blog-detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+
+from app.models import Blog
 
 
-class IndexView(TemplateView):
-    template_name = 'app/index.html'
+def index_view(request):
+    return render(request=request,
+                  template_name="app/index.html")
+
+
+def blogs_view(request):
+    blogs = Blog.objects.all()[:3]
+    return render(request=request,
+                  template_name="app/blog_main/blogs.html",
+                  context={"blogs":blogs})
+
+
+def blog_detail(request,blog_id):
+    blog_single = Blog.objects.filter(id=blog_id).first()
+    return render(request=request,
+                  template_name="app/blog_main/blog-detail.html",
+                  context={"blog_single": blog_single})
