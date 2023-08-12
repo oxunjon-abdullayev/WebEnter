d57b31d57b4fd79f528fb315fd953e1774c5c2e1
oxunjon <abdullayevoxunjon757@gmail.com>
2023-08-13 01:16:43 +0500

    blog blog deatail is done

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
+  
 
 class Blog(models.Model):
     title = models.CharField(max_length=155)
     description = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to="blog/")
-
+           
 
--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from app.views import IndexView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView, name='index')
 ]
